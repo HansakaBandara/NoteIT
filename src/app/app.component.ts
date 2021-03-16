af2c180742d1d7ae5a2f9a6e96af79da7c35d127
hansakab <hmtbv8@gmail.com>
2021-03-16 09:12:39 +0530

    NoteIT sheduler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import{View} from '@syncfusion/ej2-angular-schedule'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<ejs-schedule [currentView]="setView"></ejs-schedule>',
+  //templateUrl: './app.component.html',     // commented templateUrl
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'noteit';
+  title = 'noteit'; 
+  public setView: View = 'Month';  // define month view as defult view
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ScheduleModule, RecurrenceEditorModule, DayService,WeekService,MonthService,MonthAgendaService,WorkWeekService,AgendaService } from '@syncfusion/ej2-angular-schedule';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScheduleModule, RecurrenceEditorModule
   ],
-  providers: [],
+  providers: [DayService,WeekService,MonthService,MonthAgendaService,WorkWeekService,AgendaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
